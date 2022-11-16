dff8accc6316d94efaa560c5923c3346f27c80a6
2201111-kishikawa <114889553+2201111-kishikawa@users.noreply.github.com>
2022-11-16 09:49:04 +0900

    フォームの追加
    パスワードとメールアドレスの追加

--- login.php
+++ login.php
@@ -1,1 +1,13 @@
-
+<html>
+  <form action="/ProductList.html" method="post">
+    <div>
+         <label for="email">メールアドレス</label>
+         <input type="text" id="email" name="email">
+    </div>
+    <div>
+         <label for="password">パスワード</label>
+         <input type="text" id="password" name="password">
+    </div>
+    <input type="submit" value="送信する">
+  </form>
+</html>
